ee3500d61ae5c350e69b94e827cd5833f18ae841
5usu <srdwivedi_b23@tx.vjti.ac.in>
2024-03-02 22:11:38 +0530

    changes done

--- app/(routes)/contact/page.jsx
+++ app/(routes)/contact/page.jsx
@@ -15,9 +15,11 @@ const ContactUsPage = () => {
       </div>
       <div className="max-w-md mx-auto bg-black p-8 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4">Contact Information</h2>
-        <p className="mb-4">Atharva Joshi</p>
+        <p className="mb-4">Subodh Mhanmunkar (General Secretary ):-  +91 93268 61879</p>
+        <p className="mb-4">Swaraj Pawar (Joint Secretary):- +91 86557 37747</p>
+        <p className="mb-4">Dr. Arvind Bhongade (Faculty Co-ordinatoe) :- +91 97023 77290</p>
+        <p className="mb-4">Dr. Deepa Rajsinghania (Diploma Faculty Co-ordinator)  +91 98205 60567</p>
         <p className="mb-4">Email: vastra2020@gmail.com</p>
-        <p className="mb-4">Number: 8956955998</p>
       </div>
       <Footer/>
     </div>
